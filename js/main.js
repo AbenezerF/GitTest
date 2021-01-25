4d3ca53674edcb0c0f0051019d335be71dca818c
Abenezer Fesseha <abenezerfesseha@Abenezers-MacBook-Pro.local>
2021-01-25 12:50:32 -0500

    please work

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+maths.addEventListener('click', calc);
+
+function calc(){
+    let num1 = Number(document.querySelector('.num1').value);
+    let num2 = Number(document.querySelector('.num2').value);
+    let result = num1/num2;
+    console.log(result);
+}
